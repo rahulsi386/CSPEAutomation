e7c7e8f4c54cc47483f10b5390ab6730373db27f
Rahul Prashant Singh <rahulsi@microsoft.com>
2020-08-27 18:30:59 +0530

    Add project files.

---
+++ CloudNinja/Dialogs/RootDialog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace CloudNinja.Dialogs
+{
+    public class RootDialog<T> : ComponentDialog where T : Dialog
+    {
+        public RootDialog()
+        {
+
+        }
+    }
+}
